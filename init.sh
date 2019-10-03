e637acce39792157d4d2ce4f042c517f9e4d74db
Jérémy Lejeune <jeremy.lejeune@zenika.com>
2019-10-03 09:50:24 +0200

    :loud_sound: add logs (#8)

--- init.sh
+++ init.sh
@@ -18,6 +18,7 @@ echo "Don't order before: ${NOT_BEFORE}"
 
 [[ -f next-orders.cb ]] && echo "File for next orders found" || touch next-orders.cb
 [[ -f sent-orders.cb ]] && echo "File for sent orders found" || touch sent-orders.cb
+[[ -f logfile.log ]] && echo "Logs file found" || touch logfile.log
 
 if [[ `tail -1 next-orders.cb` < $CURRENT_DATE ]];then
 	curl -H "Authorization: Bearer ${WEBHOOK_TOKEN}" \
@@ -27,14 +28,14 @@ if [[ `tail -1 next-orders.cb` < $CURRENT_DATE ]];then
 	if [[ $? == 0 ]];then
 		echo "\n$NOT_BEFORE" > next-orders.cb
 		echo "\n$CURRENT_DATE" > sent-orders.cb
-		echo "Command sent"
+		echo "`date` -- Command sent\n" > logfile.log
 		python led_ok.py
 	else
-		echo "Something went wrong!"
+		echo "`date` -- Something went wrong!" > logfile.log
 		python led_ko.py
 	fi
 else
-	echo "Order already placed"
+	echo "`date` -- Order already placed" > logfile.log
 	python led_ok.py
 fi
 
