8c6476a5e3f8d421e088ee4e77aeaada336ada53
Jérémy Lejeune <jeremy.lejeune@zenika.com>
2019-10-03 17:06:22 +0200

    add path for files

--- init.sh
+++ init.sh
@@ -16,29 +16,32 @@ python ${SCRIPTPATH}/led_boot.py
 echo "Current date and time is : ${CURRENT_DATE}"
 echo "Don't order before: ${NOT_BEFORE}"
 
-[[ -f next-orders.cb ]] && echo "File for next orders found" \
-	|| touch next-orders.cb && echo "File for next orders created"
-[[ -f sent-orders.cb ]] && echo "File for sent orders found" \
-	|| touch sent-orders.cb && echo "File for sent orders created"
-[[ -f logfile.log ]] && echo "Logs file found" \
-	|| touch logfile.log && echo "Log file created"
+[[ -d /etc/coffee-button ]] && echo "Directory exists" \
+	|| mkdir -p /etc/coffee-button && echo "Directory created"
+FILE_DIR=/etc/coffee-button
+[[ -f ${FILE_DIR}/next-orders.cb ]] && echo "File for next orders found" \
+	|| touch ${FILE_DIR}/next-orders.cb && echo "File for next orders created"
+[[ -f ${FILE_DIR}/sent-orders.cb ]] && echo "File for sent orders found" \
+	|| touch ${FILE_DIR}/sent-orders.cb && echo "File for sent orders created"
+[[ -f ${FILE_DIR}/logfile.log ]] && echo "Logs file found" \
+	|| touch ${FILE_DIR}/logfile.log && echo "Log file created"
 
-if [[ $(tail -1 next-orders.cb) < $CURRENT_DATE ]];then
+if [[ $(tail -1 ${FILE_DIR}/next-orders.cb) < $CURRENT_DATE ]];then
 	curl -H "Authorization: Bearer ${WEBHOOK_TOKEN}" \
 		-X POST \
 		"${WEBHOOK_URL}" \
 		--data "order-date=${CURRENT_DATE}"
 	if [[ $? == 0 ]];then
-		printf "\n$NOT_BEFORE" > next-orders.cb
-		printf "\n$CURRENT_DATE" > sent-orders.cb
-		echo "$(date) -- Command sent\n" >> logfile.log
+		printf "\n$NOT_BEFORE" > ${FILE_DIR}/next-orders.cb
+		printf "\n$CURRENT_DATE" > ${FILE_DIR}/sent-orders.cb
+		echo "$(date) -- Command sent\n" >> ${FILE_DIR}/logfile.log
 		python ${SCRIPTPATH}/led_ok.py
 	else
-		echo "$(date) -- Something went wrong!" >> logfile.log
+		echo "$(date) -- Something went wrong!" >> ${FILE_DIR}/logfile.log
 		python ${SCRIPTPATH}/led_ko.py
 	fi
 else
-	echo "$(date) -- Order already placed" >> logfile.log
+	echo "$(date) -- Order already placed" >> ${FILE_DIR}/logfile.log
 	python ${SCRIPTPATH}/led_ok.py
 fi
 
