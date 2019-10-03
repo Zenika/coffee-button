91be4af8a3e68db538eb0197022bc67c665ee368
Jérémy Lejeune <jeremy.lejeune@zenika.com>
2019-10-03 16:20:42 +0200

    :recycle: shellcheck corrections (#11)

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
-#! /bin/bash
+#! /bin/bash -e
 
 # This script is supposed to:
 # - check the date and compare to the last usage of the button
 # - if more than 48h as passed since last use, send a POST request using curl to Zappier URL
 # - this request contains an Authorization header and the current date
-# - it waits for a 200 response or retries to send request
 # - it then needs to shutdown the device
-source ./.env
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ${SCRIPTPATH}/.env
 
-CURRENT_DATE=`date +"%F-%H:%M"`
-NOT_BEFORE=`date -v+2d +"%F-%H:%M"`
+CURRENT_DATE=$(date +"%F-%H:%M")
+NOT_BEFORE=$(date -d "+2 days" +"%F-%H:%M")
 
-python led_boot.py
+python ${SCRIPTPATH}/led_boot.py
 
 echo "Current date and time is : ${CURRENT_DATE}"
 echo "Don't order before: ${NOT_BEFORE}"
@@ -20,23 +20,23 @@ echo "Don't order before: ${NOT_BEFORE}"
 [[ -f sent-orders.cb ]] && echo "File for sent orders found" || touch sent-orders.cb
 [[ -f logfile.log ]] && echo "Logs file found" || touch logfile.log
 
-if [[ `tail -1 next-orders.cb` < $CURRENT_DATE ]];then
+if [[ $(tail -1 next-orders.cb) < $CURRENT_DATE ]];then
 	curl -H "Authorization: Bearer ${WEBHOOK_TOKEN}" \
 		-X POST \
 		"${WEBHOOK_URL}" \
 		--data "order-date=${CURRENT_DATE}"
 	if [[ $? == 0 ]];then
-		echo "\n$NOT_BEFORE" > next-orders.cb
-		echo "\n$CURRENT_DATE" > sent-orders.cb
-		echo "`date` -- Command sent\n" > logfile.log
-		python led_ok.py
+		printf "\n$NOT_BEFORE" > next-orders.cb
+		printf "\n$CURRENT_DATE" > sent-orders.cb
+		echo "$(date) -- Command sent\n" >> logfile.log
+		python ${SCRIPTPATH}/led_ok.py
 	else
-		echo "`date` -- Something went wrong!" > logfile.log
-		python led_ko.py
+		echo "$(date) -- Something went wrong!" >> logfile.log
+		python ${SCRIPTPATH}/led_ko.py
 	fi
 else
-	echo "`date` -- Order already placed" > logfile.log
-	python led_ok.py
+	echo "$(date) -- Order already placed" >> logfile.log
+	python ${SCRIPTPATH}/led_ok.py
 fi
 
-sudo power off
+sudo poweroff
