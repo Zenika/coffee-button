2ec5679f38337769f44a5865deef39939a1b471e
Jérémy Lejeune <jeremy.lejeune@zenika.com>
2019-10-25 14:53:09 +0200

    Add duration variable and checks

--- init.sh
+++ init.sh
@@ -6,27 +6,36 @@
 # - this request contains an Authorization header and the current date
 # - it then needs to shutdown the device
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# Files and directory check and/or creation
 [[ -d /etc/coffee-button ]] && echo "Directory exists" \
 	|| (mkdir -p /etc/coffee-button && echo "Directory created")
 FILE_DIR="/etc/coffee-button"
 [[ -f ${FILE_DIR}/.env ]] && echo "Env file found" \
 	|| (cp /home/pi/coffee-button/.env ${FILE_DIR} && echo "Env file copied from last commit")
-source ${FILE_DIR}/.env
-
-CURRENT_DATE=$(date +"%F-%H:%M")
-NOT_BEFORE=$(date -d "+2 days" +"%F-%H:%M")
-
-python ${SCRIPTPATH}/led_boot.py
-
-echo "Current date and time is : ${CURRENT_DATE}"
-echo "Don't order before: ${NOT_BEFORE}"
-
 [[ -f ${FILE_DIR}/next-orders.cb ]] && echo "File for next orders found" \
 	|| (touch ${FILE_DIR}/next-orders.cb && echo "File for next orders created")
 [[ -f ${FILE_DIR}/sent-orders.cb ]] && echo "File for sent orders found" \
 	|| (touch ${FILE_DIR}/sent-orders.cb && echo "File for sent orders created")
 [[ -f ${FILE_DIR}/logfile.log ]] && echo "Logs file found" \
 	|| (touch ${FILE_DIR}/logfile.log && echo "Log file created")
+
+# Importing env and testing duration value
+source ${FILE_DIR}/.env
+REGEXP='^[0-9]+$'
+if ! [[ $DURATION_IN_DAYS =~ $REGEXP ]] ; then
+	echo "Error: Duration in days is not a number" >> ${FILE_DIR}/logfile.log
+	python ${SCRIPTPATH}/led_ko.py
+	exit 1
+fi
+
+CURRENT_DATE=$(date +"%F-%H:%M")
+NOT_BEFORE=$(date -d "+$DURATION_IN_DAYS days" +"%F-%H:%M")
+
+python ${SCRIPTPATH}/led_boot.py
+
+echo "Current date and time is : ${CURRENT_DATE}"
+echo "Don't order before: ${NOT_BEFORE}"
 
 if [[ $(tail -1 ${FILE_DIR}/next-orders.cb) < $CURRENT_DATE ]];then
 	curl -H "Authorization: Bearer ${WEBHOOK_TOKEN}" \
